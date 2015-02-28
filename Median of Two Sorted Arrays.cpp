2362da4e5b70018b4e9d2ae6956f273b8c25cb40
System Administrator <root@Changleis-MacBook-Pro.local>
2015-02-28 16:07:06 -0500

    Median of two Sorted Arrays of Leetcode.2

---
+++ Median of Two Sorted Arrays.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        int temp = (m + n)/2;
+        int Is_odd = m + n - (m + n)/2 - (m + n)/2;
+        double result;
+ 
+        if(!Is_odd)
+        {
+            result = (double)Search(A, m, B, n, temp) + (double)Search(A, m, B, n, temp + 1);
+            return result/2;
+        }
+        else
+        {
+            result = (double)Search(A, m, B, n, temp + 1);
+            return result;
+        }
+    }
+    
+    int Search(int a[], int n, int b[], int m, int k)  
+    {  
+            //assert(a && b);   
+            if (n <= 0) return b[k-1];  
+            if (m <= 0) return a[k-1];  
+            if (k <= 1) return min(a[0], b[0]);   
+            if (b[m/2] >= a[n/2])  
+            {  
+                 if ((n/2 + 1 + m/2) >= k)  
+                     return Search(a, n, b, m/2, k);  
+                 else  
+                      return Search(a + n/2 + 1, n - (n/2 + 1), b, m, k - (n/2 + 1));  
+            }  
+            else  
+            {  
+                if ((m/2 + 1 + n/2) >= k)  
+                      return Search( a, n/2,b, m, k);  
+                else  
+                      return Search( a, n, b + m/2 + 1, m - (m/2 + 1),k - (m/2 + 1));  
+            }  
+       } 
+};
